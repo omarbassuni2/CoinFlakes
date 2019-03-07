4334b8423a72ea80c300478f45f6abe63e612eac
Omar Bassuni <44812986+omarbassuni2@users.noreply.github.com>
2019-03-07 06:31:26 +0200

    Add files via upload

--- CoinFlakes/src/BlockChain.java
+++ CoinFlakes/src/BlockChain.java
@@ -1,4 +1,52 @@
+import java.util.ArrayList;
 
 public class BlockChain {
-
+	
+	private static ArrayList<Block> Chain;
+	private static ArrayList<BlockChain> allChains = new ArrayList<BlockChain>();
+	public BlockChain(){
+		Chain = new ArrayList<Block>();
+		allChains.add(this);
+	}
+	public BlockChain(Block blk){
+		Chain = new ArrayList<Block>();
+		addBlock(blk);
+		allChains.add(this);
+	}
+	public BlockChain(String dummyParameter){
+		
+	}
+	public void addBlock(Block blk){
+		if(BlockChain.Chain.size() == 0){
+			blk.setPrevHash("NULL");
+			blk.setIndex(Chain.size());
+			Chain.add(blk);
+		}
+		else{
+			Block getPreHashBlock = Chain.get(Chain.size()-1);
+			blk.setPrevHash(getPreHashBlock.getHash());
+			blk.setIndex(Chain.size());
+			Chain.add(blk);			
+		}	
+	}
+	public void displayChain(){
+		for(int i = 0;i < Chain.size(); i++){
+			System.out.println("Block: " + i);
+			Chain.get(i).getData().displayInfo();
+			System.out.println("Hash: " + Chain.get(i).getHash());
+			System.out.println("PrevHash: " + Chain.get(i).getPrevHash());
+		}
+	}
+	
+	public void setChains(ArrayList<BlockChain> chains){
+		BlockChain.allChains = chains;
+	}
+	public ArrayList<BlockChain> getChains(){
+		return allChains;
+	}
+	public void displayAllChains(){
+		for(int i = 0;i < allChains.size();i++){
+			allChains.get(i).displayChain();
+		}
+	}	
 }
--- CoinFlakes/src/User.java
+++ CoinFlakes/src/User.java
@@ -1,9 +1,30 @@
+import java.util.Scanner;
 
 public class User {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		@SuppressWarnings("unused")
+		Wallet newWalletUser = new Wallet();
+		BlockChain chainsViewer = new BlockChain("DummyObject");
+		Scanner scanner = new Scanner(System.in);
+		while(true){
+			System.out.println("1)Send Coins\n2)Check your blockchains\n3)Check all blockchains\n4)Exist");
+			int choice = scanner.nextInt();
+			if(choice == 1){
+				WalletServices useSendFunc = new WalletServices();
+				useSendFunc.send();
+			}
+			if(choice == 2){
+				chainsViewer.displayChain();
+			}
+			if(choice == 3){
+				chainsViewer.displayAllChains();
+			}
+			if(choice == 4){
+				break;
+			}
+		}
+		scanner.close();
 	}
 
 }
